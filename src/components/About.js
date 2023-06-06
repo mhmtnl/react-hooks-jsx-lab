bbe136452fa959fc595dc13602d84ec74d1210a6
mehmetunlu <munlu4037@gmail.com>
2023-06-06 16:34:02 +0300

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>Our environment includes all living and non-living things on our planet earth. Earth is a beautiful place with lavish green lands, diverse species, oceans and natural resources. The valuable things in the natural environment are natural resources. But over the past few decades global warming is causing serious damage to our environment.</p> 
+    <img src={image} alt="I made this"></img>
+  </div>;
 }
 
 export default About;
